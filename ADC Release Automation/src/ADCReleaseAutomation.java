493f163b2f3487d6239f2f5019bd7d42bf1d8ca9
Kayler Renslow <kaylerrenslow@gmail.com>
2017-08-15 00:27:27 -0500

    trying to fix release automation

--- ADC Release Automation/src/ADCReleaseAutomation.java
+++ ADC Release Automation/src/ADCReleaseAutomation.java
@@ -100,7 +100,8 @@ public class ADCReleaseAutomation {
 
 		for (File f : filesToPack) {
 			try {
-				File dest = new File("out/production/ADC Installer/install/" + f.getName());
+				File dest = new File(workingDirectoryPath + "/out/production/ADC Installer/install/" + f.getName());
+				System.out.println("Copying " + f.toPath() + " to '" + dest.toPath() + "'");
 				Files.copy(f.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
