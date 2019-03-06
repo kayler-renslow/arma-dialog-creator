ad0822c5c8ea909191b1e42ba741b397ca452de1
Kayler Renslow <kaylerrenslow@gmail.com>
2019-03-05 23:09:44 -0600

    use dark theme menu item now does something

--- src/com/armadialogcreator/gui/main/actions/mainMenu/view/ViewDarkThemeAction.java
+++ src/com/armadialogcreator/gui/main/actions/mainMenu/view/ViewDarkThemeAction.java
@@ -1,10 +1,13 @@
 package com.armadialogcreator.gui.main.actions.mainMenu.view;
 
+import com.armadialogcreator.ArmaDialogCreator;
 import com.armadialogcreator.data.ApplicationProperties;
 import com.armadialogcreator.data.Settings;
 import com.armadialogcreator.gui.fxcontrol.MenuItemEventHandler;
+import com.armadialogcreator.gui.styles.ADCStyleSheets;
 import javafx.event.ActionEvent;
 import javafx.scene.control.CheckMenuItem;
+import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -24,9 +27,32 @@ public class ViewDarkThemeAction implements MenuItemEventHandler<CheckMenuItem> 
 		useDarkTheme.not();
 		source.setSelected(useDarkTheme.isTrue());
 
-		//ArmaDialogCreator.setToDarkTheme(checked);
+		final String darkTheme = ADCStyleSheets.getStylesheet("dark.css");
+		Stage stage = ArmaDialogCreator.getPrimaryStage();
+		if (useDarkTheme.isTrue()) {
+			stage.getScene().getStylesheets().add(darkTheme);
+		} else {
+			stage.getScene().getStylesheets().remove(darkTheme);
+		}
 	}
 
+	//	public static void setToDarkTheme(boolean set) {
+	//		final String darkTheme = ADCStyleSheets.getStylesheet("dark.css");
+	//		if (set) {
+	//			CanvasViewColors.EDITOR_BG = CanvasViewColors.DARK_THEME_EDITOR_BG;
+	//			CanvasViewColors.GRID = CanvasViewColors.DARK_THEME_GRID;
+	//			INSTANCE.primaryStage.getScene().getStylesheets().add(darkTheme);
+	//		} else {
+	//			CanvasViewColors.EDITOR_BG = CanvasViewColors.DEFAULT_EDITOR_BG;
+	//			CanvasViewColors.GRID = CanvasViewColors.DEFAULT_GRID;
+	//			INSTANCE.primaryStage.getScene().getStylesheets().remove(darkTheme);
+	//		}
+	//		if (getADCWindow().isShowing()) {
+	//			getCanvasView().updateCanvas();
+	//		}
+	//		getApplicationDataManager().getApplicationProperties().put(ApplicationProperty.DARK_THEME, set);
+	//		getApplicationDataManager().saveApplicationProperties();
+	//	}
 	@Override
 	public void setMenuItem(@NotNull CheckMenuItem menuItem) {
 		menuItem.setSelected(useDarkTheme.isTrue());
