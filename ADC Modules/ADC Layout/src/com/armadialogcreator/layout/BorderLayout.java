f419d5de532385957477b659ab54bea3a19c8319
kayler <kaylerrenslow@gmail.com>
2019-10-11 08:51:38 -0500

    working on border layout

--- ADC Modules/ADC Layout/src/com/armadialogcreator/layout/BorderLayout.java
+++ ADC Modules/ADC Layout/src/com/armadialogcreator/layout/BorderLayout.java
@@ -1,7 +1,38 @@
 package com.armadialogcreator.layout;
 
+import com.armadialogcreator.util.ListObserver;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 /**
  @author Kayler
  @since 7/23/19. */
 public class BorderLayout implements Layout {
+	private final ListObserver<LayoutNode> children = new ListObserver<>(new ArrayList<>());
+
+	public BorderLayout() {
+		children.addListener(new LayoutChildrenListener(this));
+	}
+
+	@Override
+	public void recomputePositions() {
+
+	}
+
+	@Override
+	@NotNull
+	public ListObserver<LayoutNode> getChildren() {
+		return children;
+	}
+
+	@Override
+	public @NotNull String getName() {
+		return "Border Layout";
+	}
+
+	@Override
+	public void invalidate() {
+		children.invalidate();
+	}
 }
--- ADC Modules/ADC Layout/src/com/armadialogcreator/layout/LayoutNode.java
+++ ADC Modules/ADC Layout/src/com/armadialogcreator/layout/LayoutNode.java
@@ -9,15 +9,16 @@ public interface LayoutNode {
 
 
 	/**
-	 Assigns this node a {@link Bounds} instance. This method is invoked when this {@link LayoutNode} is added to a {@link Layout}'s
+	 Assigns this node a {@link NodeBounds} instance.
+	 This method is invoked when this {@link LayoutNode} is added to a {@link Layout}'s
 	 children via {@link Layout#getChildren()}
 
 	 @param bounds the bounds assigned to this node
 	 */
-	void assignBounds(@NotNull Bounds bounds);
+	void assignBounds(@NotNull NodeBounds bounds);
 
-	/** @return the {@link Bounds} instance passed through {@link #assignBounds(Bounds)} */
+	/** @return the {@link Bounds} instance passed through {@link #assignBounds(NodeBounds)} */
 	@NotNull
-	Bounds getBounds();
+	NodeBounds getBounds();
 
 }
